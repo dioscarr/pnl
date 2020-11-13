f7e34dfd5cb4dcc35e787e0fc69e830beda52bc2
dioscar Rodriguez <dioscarr@gmail.com>
2020-11-13 11:56:58 -0500

    changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,24 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
 
 
 
-hidePanel();
-function showPanel() {
-    var taxPanel = document.getElementsByClassName("view-taxes-by-year");
-    taxPanel.show();
-
-}
-
-function hidePanel() {
-    var taxPanel = document.getElementsByClassName("view-taxes-by-year");
-    taxPanel.hide();
-
-}
-
-document.getElementById("viewtaxesbtn").addEventListener("click", function () {
-    taxPanel();
-})
+    $(document).ready(function()
+    {
+        var viewTaxPanel = $(".view-my-taxes");
+        $(viewTaxPanel).on("click", function(){
+            alert();            
+        });
+    })
 
