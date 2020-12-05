6e03c7cbdac3cbb0fbd7ff886c13e63e237fe9c8
dioscar Rodriguez <dioscarr@gmail.com>
2020-12-05 18:26:09 -0500

    mistake and did not saved

---
+++ Data/Models/PagesConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pnl.Data.Models
+{
+    public class PagesConfig
+    {
+        public HtmlString Title { get; set; } = new HtmlString("<p>default content</p>");
+        public HtmlString TopSubTitle { get; set; } = new HtmlString("<p>default Contrent</p>");
+        public HtmlString Message { get; set; } = new HtmlString("<p>Message</p>");
+    }
+}
---
+++ Migrations/20201204161427_taxFormProgress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pnl.Migrations
+{
+    public partial class taxFormProgress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) { 
+
+            migrationBuilder.AddColumn<int>(
+                name: "Progress",
+                table: "TaxForms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Progress",
+                table: "TaxForms");          
+        }
+    }
+}
